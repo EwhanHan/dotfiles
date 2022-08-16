134b81cefed3eea4392d923f2610444151f3de0a
Ewhan Han <ewhan.han@gmail.com>
2022-08-16 14:25:03 -0400

    Add prereq script

---
+++ prerequisite/homebrew.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# Might as well ask for password up-front, right?
+sudo -v
+
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
